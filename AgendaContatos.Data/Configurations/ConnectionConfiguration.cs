897af6c9a8b85092a275525297a0a06c0075d351
André Rodrigues <andre.rodriguesneves@gmail.com>
2022-09-06 10:13:43 -0300

    first commit

---
+++ AgendaContatos.Data/Configurations/ConnectionConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaContatos.Data.Configurations
+{
+    public class ConnectionConfiguration
+    {
+        public static string GetConnectionString()
+        {
+            return @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DB_AGENDACONTATOS;Integrated Security=True;
+                    Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;
+                    ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        }
+    }
+}
